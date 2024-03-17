6a28a4c23395845f7b3983ed6e5711a0b3eb57ce
anubhootipandey <anubhootipandey05@gmail.com>
2024-03-17 11:48:34 +0530

    searching added

--- src/components/TodoTask.js
+++ src/components/TodoTask.js
@@ -1,44 +1,61 @@
-import React,{useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import "../App.css";
 
-const TodoTask = ({formData, setFormData}) => {
-  //optional chaining
+const TodoTask = ({ formData, setFormData }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
   useEffect(() => {
     const storedFormData = localStorage.getItem('key');
     if (storedFormData) {
-        setFormData(JSON.parse(storedFormData));
+      setFormData(JSON.parse(storedFormData));
     }
-}, [setFormData]);
+  }, [setFormData]);
+
+  const getFilteredData = () => {
+    return formData.filter(item =>
+      item.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      item.description.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }
 
   const getArrayData = () => {
-    const data = formData.map((item) => {
-      return <tr>
-      <td>{item.title}</td>
-      <td>{item.description}</td>
-      <td>{item.dueDate}</td>
-      <td>{item.priority}</td>
+    const filteredData = getFilteredData();
+    return filteredData.map((item, index) => (
+      <tr key={index}>
+        <td>{item.title}</td>
+        <td>{item.description}</td>
+        <td>{item.dueDate}</td>
+        <td>{item.priority}</td>
       </tr>
-    })
-    return data;
+    ));
   }
-  console.log("result", formData);
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  }
+
   return (
     <div className='todo-container'>
-    <h3>List of todo item:</h3>
-    <input type='search' placeholder='Search by task title or description' />
-    <table id='data'>
-    <thead>
-    <tr>
-        <th>Title</th>
-        <th>Description</th>
-        <th>Due Date</th>
-        <th>Priority</th>
-    </tr>
-    </thead>
-    <tbody>
-      {getArrayData()}
-    </tbody>
-    </table>
+      <h3>List of todo item:</h3>
+      <input
+        type='search'
+        placeholder='Search by task title or description'
+        value={searchQuery}
+        onChange={handleSearchChange}
+      />
+      <table id='data'>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Due Date</th>
+            <th>Priority</th>
+          </tr>
+        </thead>
+        <tbody>
+          {getArrayData()}
+        </tbody>
+      </table>
     </div>
   );
 }
