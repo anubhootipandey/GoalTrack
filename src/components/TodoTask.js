8f61dabb0bf574a2203165e1e37c092a608b2e42
anubhootipandey <anubhootipandey05@gmail.com>
2024-05-11 22:13:11 +0530

    added edit functionality

--- src/components/TodoTask.js
+++ src/components/TodoTask.js
@@ -3,6 +3,11 @@ import "../App.css";
 
 const TodoTask = ({ formData, setFormData }) => {
   const [searchQuery, setSearchQuery] = useState('');
+  const [editingIndex, setEditingIndex] = useState(null);
+  const [editedTitle, setEditedTitle] = useState('');
+  const [editedDescription, setEditedDescription] = useState('');
+  const [editedDueDate, setEditedDueDate] = useState('');
+  const [editedPriority, setEditedPriority] = useState('');
 
   useEffect(() => {
     const storedFormData = localStorage.getItem('key');
@@ -25,15 +30,46 @@ const TodoTask = ({ formData, setFormData }) => {
     localStorage.setItem('key', JSON.stringify(updatedFormData));
   };
 
+  const handleEditTask = (index) => {
+    setEditingIndex(index);
+    const taskToEdit = formData[index];
+    setEditedTitle(taskToEdit.title);
+    setEditedDescription(taskToEdit.description);
+    setEditedDueDate(taskToEdit.dueDate);
+    setEditedPriority(taskToEdit.priority);
+  };
+
+  const handleSaveEdit = () => {
+    const updatedFormData = [...formData];
+    updatedFormData[editingIndex] = {
+      title: editedTitle,
+      description: editedDescription,
+      dueDate: editedDueDate,
+      priority: editedPriority
+    };
+    setFormData(updatedFormData);
+    localStorage.setItem('key', JSON.stringify(updatedFormData));
+    setEditingIndex(null);
+  };
+
   const getArrayData = () => {
     const filteredData = getFilteredData();
     return filteredData.map((item, index) => (
       <tr key={index}>
-        <td>{item.title}</td>
-        <td>{item.description}</td>
-        <td>{item.dueDate}</td>
-        <td>{item.priority}</td>
-        <td><button onClick={() => handleDeleteTask(index)}>Delete</button></td>
+        <td>{index === editingIndex ? <input type="text" value={editedTitle} onChange={(e) => setEditedTitle(e.target.value)} /> : item.title}</td>
+        <td>{index === editingIndex ? <input type="text" value={editedDescription} onChange={(e) => setEditedDescription(e.target.value)} /> : item.description}</td>
+        <td>{index === editingIndex ? <input type="date" value={editedDueDate} onChange={(e) => setEditedDueDate(e.target.value)} /> : item.dueDate}</td>
+        <td>{index === editingIndex ? <input type="text" value={editedPriority} onChange={(e) => setEditedPriority(e.target.value)} /> : item.priority}</td>
+        <td>
+          {index === editingIndex ? (
+            <button onClick={handleSaveEdit}>Save</button>
+          ) : (
+            <>
+              <button onClick={() => handleEditTask(index)}>Edit</button>
+              <button onClick={() => handleDeleteTask(index)}>Delete</button>
+            </>
+          )}
+        </td>
       </tr>
     ));
   }
@@ -52,20 +88,22 @@ const TodoTask = ({ formData, setFormData }) => {
         onChange={handleSearchChange}
       />
       
-      <table id='data'>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Due Date</th>
-            <th>Priority</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {getArrayData()}
-        </tbody>
-      </table>
+      <div className="table-wrapper">
+        <table id='data'>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Due Date</th>
+              <th>Priority</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {getArrayData()}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
